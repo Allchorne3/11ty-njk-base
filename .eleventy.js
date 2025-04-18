d91352cbff3e6d474eb2d37c82a3a38f84500a0a
Stefan Allchorne <allchorne39@sky.com>
2025-04-18 19:21:59 +0100

    Cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -4,70 +4,70 @@ const prettyDate = require('./lib/pretty-date.js')
 
 module.exports = function(eleventyConfig) {
 
-    // Recomplile 11ty when files change
-    eleventyConfig.addWatchTarget("./src/style/")
+	// Recomplile 11ty when files change
+	eleventyConfig.addWatchTarget("./src/style/")
 
-    eleventyConfig.addCollection("featuredPosts", collection => {
-        return collection.getFilteredByTag("post").filter(item => item.data.featured);
-    });
+	eleventyConfig.addCollection("featuredPosts", collection => {
+		return collection.getFilteredByTag("post").filter(item => item.data.featured);
+	});
 
-    // Expose Nunjucks filters
-    eleventyConfig.addFilter("prettyDate", prettyDate);
+	// Expose Nunjucks filters
+	eleventyConfig.addFilter("prettyDate", prettyDate);
 
-    // Automatically open up the browser on script runs
-    eleventyConfig.setBrowserSyncConfig({
-        open: true
-    })
+	// Automatically open up the browser on script runs
+	eleventyConfig.setBrowserSyncConfig({
+		open: true
+	})
 
-    // minify html on prod build
-    eleventyConfig.addTransform ('htmlmin', content => {
-        if (process.env.NODE_ENV === 'production') {
-            return htmlmin.minify (content, {
-                useShortDoctype: true,
-                removeComments: true,
-                collapseWhitespace: true,
-            })
-        }
-        return content
-    })
+	// minify html on prod build
+	eleventyConfig.addTransform ('htmlmin', content => {
+		if (process.env.NODE_ENV === 'production') {
+			return htmlmin.minify (content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+			})
+		}
+		return content
+	})
 
-    eleventyConfig.addGlobalData('site', {
-        name: 'Your Site Name',
-        url: 'https://yoursitename.com',
-        host: process.env.NODE_ENV === 'production' ? 'https://gearset.com' : 'http://localhost:8080',
-        slogan: 'Your global site slogan',
-        description: "your default site description"
-    });
+	eleventyConfig.addGlobalData('site', {
+		name: 'Your Site Name',
+		url: 'https://yoursitename.com',
+		host: process.env.NODE_ENV === 'production' ? 'https://your-site.com' : 'http://localhost:8080',
+		slogan: 'Your global site slogan',
+		description: "your default site description"
+	});
 
-    return {
-        htmlTemplateEngine: 'njk',
+	return {
+		htmlTemplateEngine: 'njk',
 
-        dir: {
-            input: "site/src",
-            output: "public",
-            // In relation to input
-            layouts: "../templates",
-            // In relation to input
-            includes: "../templates/partials"
-        },
+		dir: {
+			input: "site/src",
+			output: "public",
+			// In relation to input
+			layouts: "../templates",
+			// In relation to input
+			includes: "../templates/partials"
+		},
 
-        templateFormats: [
-            "md",
-            "njk",
-            "jpg",
-            "jpeg", 
-            "png",
-            "gif",
-            "webp",
-            "svg",
-            "xml",
-            "txt",
-            "pdf",
-            "zip",
-            "mp4",
-            "ogv",
-            "json",
-            "ics"
-        ]
-    }
+		templateFormats: [
+			"md",
+			"njk",
+			"jpg",
+			"jpeg", 
+			"png",
+			"gif",
+			"webp",
+			"svg",
+			"xml",
+			"txt",
+			"pdf",
+			"zip",
+			"mp4",
+			"ogv",
+			"json",
+			"ics"
+		]
+	}
 }
--- site/src/scripts/app.js
+++ site/src/scripts/app.js
@@ -1,8 +1,6 @@
 const moment = require("moment") // For date formatting
 
 document.addEventListener('DOMContentLoaded', () => {
-    
-    // Get the current yeaer
-    document.getElementById('is-year').innerHTML = moment().year();
-
+	// Get the current yeaer
+	document.getElementById('is-year').innerHTML = moment().year();
 })
